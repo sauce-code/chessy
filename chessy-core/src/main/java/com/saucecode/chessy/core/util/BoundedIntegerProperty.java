621fa0a217d98cc579b07d202b2cc2f95ddbda6c
T480 <fire@web.de>
2020-04-08 23:19:57 +0200

    add javadoc

--- chessy-core/src/main/java/com/saucecode/chessy/core/util/BoundedIntegerProperty.java
+++ chessy-core/src/main/java/com/saucecode/chessy/core/util/BoundedIntegerProperty.java
@@ -2,12 +2,34 @@ package com.saucecode.chessy.core.util;
 
 import javafx.beans.property.SimpleIntegerProperty;
 
+/**
+ * A subclass of {@link SimpleIntegerProperty}, which checks given values
+ * implicit, if they are within in a certain range, given by
+ * {@link #lowerInclusive} and {@link #upperInclusive}.
+ * 
+ * @see SimpleIntegerProperty
+ * 
+ * @author Torben Kr&uumlger
+ */
 public class BoundedIntegerProperty extends SimpleIntegerProperty {
 
+	/**
+	 * Lower border, inclusive.
+	 */
 	private final int lowerInclusive;
 
+	/**
+	 * Upper border, inclusive.
+	 */
 	private final int upperInclusive;
 
+	/**
+	 * Creates a new {@link BoundedIntegerProperty}.
+	 * 
+	 * @param initialValue   initialValue
+	 * @param lowerInclusive lower border, inclusive
+	 * @param upperInclusive upper border, inclusive
+	 */
 	public BoundedIntegerProperty(int initialValue, int lowerInclusive, int upperInclusive) {
 		super(initialValue);
 		this.lowerInclusive = lowerInclusive;
