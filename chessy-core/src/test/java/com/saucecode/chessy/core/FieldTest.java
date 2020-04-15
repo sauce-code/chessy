88612a2b8d7f26afe547884dde6816201d544f6b
T480 <fire@web.de>
2020-04-15 18:43:10 +0200

    add FieldTest

---
+++ chessy-core/src/test/java/com/saucecode/chessy/core/FieldTest.java
@@ -0,0 +1,63 @@
+package com.saucecode.chessy.core;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import com.saucecode.chessy.core.FieldI.FigureType;
+import com.saucecode.chessy.core.FieldI.Modifier;
+
+class FieldTest {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void testGetter() {
+		Field field = new Field(FigureType.BISHOP_BLACK, Modifier.FROM);
+		assertEquals(FigureType.BISHOP_BLACK, field.getFigure());
+		assertEquals(Modifier.FROM, field.getModifier());
+	}
+
+	@Test
+	void testEquals() {
+		Field field1 = new Field(FigureType.PAWN_WHITE, Modifier.TO);
+		Field field2 = new Field(FigureType.PAWN_WHITE, Modifier.TO);
+		assertEquals(field1, field2);
+	}
+
+	@Test
+	void testHashEquals() {
+		Field field1 = new Field(FigureType.ROOK_BLACK, Modifier.SELECTED);
+		Field field2 = new Field(FigureType.ROOK_BLACK, Modifier.SELECTED);
+		assertEquals(field1.hashCode(), field2.hashCode());
+	}
+
+	@Test
+	void testFigureNull() {
+		assertThrows(NullPointerException.class, () -> new Field(null, Modifier.SELECTED));
+	}
+
+	@Test
+	void testModifierNull() {
+		assertThrows(NullPointerException.class, () -> new Field(FigureType.KNIGHT_WHITE, null));
+	}
+
+}
