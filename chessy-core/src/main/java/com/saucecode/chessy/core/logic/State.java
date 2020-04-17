e579096b94ca8ce8256f807760d9624239e6fe5e
T480 <fire@web.de>
2020-04-17 14:23:03 +0200

    remove toString, adjust javadoc

--- chessy-core/src/main/java/com/saucecode/chessy/core/logic/State.java
+++ chessy-core/src/main/java/com/saucecode/chessy/core/logic/State.java
@@ -18,12 +18,12 @@ public enum State {
 	CHECK_BLACK,
 
 	/**
-	 * Player white stalemated. It's a draw.
+	 * Player white is stalemated. It's a draw.
 	 */
 	STALEMATE_WHITE,
 
 	/**
-	 * Player black stalemated. It's a draw.
+	 * Player black is stalemated. It's a draw.
 	 */
 	STALEMATE_BLACK,
 
@@ -40,29 +40,6 @@ public enum State {
 	/**
 	 * No special event occured.
 	 */
-	NONE;
-
-	@Override
-	public String toString() {
-		switch (this) {
-		case CHECK_WHITE:
-			return "Player white is in check.";
-		case CHECK_BLACK:
-			return "Player black is in check.";
-		case STALEMATE_WHITE:
-			return "Player white is stalemated. It's a draw.";
-		case STALEMATE_BLACK:
-			return "Player black is stalemated. It's a draw.";
-		case CHECKMATE_WHITE:
-			return "Player white is checkmated. Player black won.";
-		case CHECKMATE_BLACK:
-			return "Player black is checkmated. Player white won.";
-		case NONE:
-			// TODO
-			return "no state";
-		default:
-			throw new IllegalArgumentException();
-		}
-	}
+	NONE,
 
 }
